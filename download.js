99b54ccf788fb26cfdd36919612cf4ddf5a6e203
Khadafi <khadafiahmad01@gmail.com>
2024-05-21 00:45:45 +0700

    perubahan pada variabel download karena kesalahan input nama buckets

--- download.js
+++ download.js
@@ -1,15 +1,15 @@
 const { Storage } = require('@google-cloud/storage');
 
 const storage = new Storage();
-const bucketName = 'cloud-storage-mlgc'
-const fileName = 'dicoding-header-logo.png';
+const bucketName = 'new-buckets-mlgc'
+const fileName = 'dicoding-header-logo.png'
 
 async function download() {
     const options = {
         destination: './image/dicoding-download.png'
     }
 
-    await storage.bucket(bucketName).file(fileName).download(options);
+    await storage.bucket(bucketName).file(fileName).download(options)
     console.log('Objek berhasil diunduh')
 }
 
